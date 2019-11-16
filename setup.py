99b5dd7a81c827c29fe959fd606ee1c20150e9b2
Antonio Augello <toni.gello94@gmail.com>
2019-11-16 10:46:07 +0000

    added package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='postcodeukfinderandformatter',
     version='0.1',
-    packages=['postcodes_io_api'],
+    packages=['postcode'],
     url='https://github.com/gello94/uk_postcode_finder_and_validator',
     license='MIT',
     author='Antonio Augello',
